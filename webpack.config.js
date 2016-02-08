a7d3d076dff304e87602c31e13be5eeba686dbda
Rafael Vidaurre <narzerus@gmail.com>
2016-02-07 21:33:37 -0300

    Basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './app/app.js',
+  }
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js',
+    chunkFilename: '[hash].bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+      {
+        test: /\.(otf|woff|woff2)(\?.+)$/,
+        loader: 'url-loader?limit=8192'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.bundle.css'),
+    new HtmlWebpackPlugin()
+  ]
+};
