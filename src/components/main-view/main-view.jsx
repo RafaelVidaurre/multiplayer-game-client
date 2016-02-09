639319e21289b171b4da2632822b10a811574ff1
Rafael Vidaurre <narzerus@gmail.com>
2016-02-09 00:07:09 -0300

    Add canvas stage

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import MainView from './components/main-view/main-view.jsx';
-
 export default React.createClass({
   render () {
     return (
-      <div id="app">
-        <MainView/>
+      <div id="main-view">
+        <canvas id="stage"></canvas>
       </div>
     );
   }
