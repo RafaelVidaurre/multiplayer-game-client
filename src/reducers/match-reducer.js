6871aec45e603c93003002bb5bce4ce0828151b6
Rafael Vidaurre <narzerus@gmail.com>
2016-02-21 18:41:07 -0300

    Create empty grid fixture

--- src/reducers/match-reducer.js
+++ src/reducers/match-reducer.js
@@ -1,8 +1,15 @@
 import combineReducers from 'redux';
 
 import {MATCH_ADD_ENTITY} from '../actions/match-actions.js';
+import emptyGrid from '../fixtures/empty-grid.js';
 
-export default function (state = {}, action) {
+const START_PHASE = 'START_PHASE';
+const INITIAL_MATCH_STATE = {
+  currentPhase: START_PHASE,
+  grid: emptyGrid()
+};
+
+export default function (state = INITIAL_MATCH_STATE, action) {
   switch (action.type) {
     case MATCH_ADD_ENTITY:
       // Here we should add an entity
