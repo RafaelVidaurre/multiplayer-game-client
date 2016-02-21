de881a1f008f903cd4aa92a983e703363546c590
Rafael Vidaurre <narzerus@gmail.com>
2016-02-21 16:53:24 -0300

    Comment

--- src/reducers/match-reducer.js
+++ src/reducers/match-reducer.js
@@ -5,7 +5,11 @@ import {MATCH_ADD_ENTITY} from '../actions/match-actions.js';
 export default function (state = {}, action) {
   switch (action.type) {
     case MATCH_ADD_ENTITY:
-
+      // Here we should add an entity
+      return state;
+    break;
+    default:
+      return state;
     break;
   }
 }
